e18e8985adcfd8ad563a2814998f70f7ea06ac84
Helgi Hrafn Gunnarsson <helgi@binary.is>
2014-01-19 22:13:08 +0000

    Tabs changed to 4 spaces in models

--- member/models.py
+++ member/models.py
@@ -2,13 +2,13 @@ from django.db import models
 from datetime import datetime
 
 class Member(models.Model):
-	kennitala = models.CharField(max_length=10, unique=True)
-	name = models.CharField(max_length=63)
-	username = models.CharField(max_length=50, unique=True)
-	email = models.EmailField(unique=True)
-	phone = models.CharField(max_length=30, blank=True)
-	partake = models.BooleanField(default=False)
-	added = models.DateTimeField(default=datetime.now)
-	mailing = models.BooleanField(default=False)
-	verified = models.BooleanField(default=False)
+    kennitala = models.CharField(max_length=10, unique=True)
+    name = models.CharField(max_length=63)
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=30, blank=True)
+    partake = models.BooleanField(default=False)
+    added = models.DateTimeField(default=datetime.now)
+    mailing = models.BooleanField(default=False)
+    verified = models.BooleanField(default=False)
 
