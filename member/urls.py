58c1324a404385f88ce6ac3e7cef3b3d0ce3f20d
Helgi Hrafn Gunnarsson <helgi@binary.is>
2014-06-03 12:34:10 +0000

    Indent fix

--- member/urls.py
+++ member/urls.py
@@ -5,20 +5,20 @@ from django.shortcuts import redirect
 from django.utils.http import urlencode
 
 urlpatterns = patterns('',
-	# Examples:
-	# url(r'^$', 'icepirate.views.home', name='home'),
-	# url(r'^icepirate/', include('icepirate.foo.urls')),
+    # Examples:
+    # url(r'^$', 'icepirate.views.home', name='home'),
+    # url(r'^icepirate/', include('icepirate.foo.urls')),
 
-	# Uncomment the admin/doc line below to enable admin documentation:
-	# url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-	# Uncomment the next line to enable the admin:
-	# url(r'^admin/', include(admin.site.urls)),
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
 
     # ------------------------------------------------------------------#
 
     # General stuff.
-	url(r'^list/(?P<group_techname>.*)', 'member.views.list', name='list'),
+    url(r'^list/(?P<group_techname>.*)', 'member.views.list', name='list'),
     url(r'^add/', 'member.views.add', name='add'),
     url(r'^edit/(?P<kennitala>[^/]+)', 'member.views.edit', name='edit'),
     url(r'^delete/(?P<kennitala>[^/]+)', 'member.views.delete', name='delete'),
@@ -32,8 +32,8 @@ urlpatterns = patterns('',
     url(r'^csv/list/(?P<group_techname>.*)', 'member.views_csv.list', name='csv_list'),
 
     # API pages (JSON)
-	url(r'^api/list/', 'member.views_api.list', name='api_list'),
-	url(r'^api/filter/(?P<field>.*)/(?P<searchstring>.+)', 'member.views_api.filter', name='api_filter'),
-	url(r'^api/get/(?P<field>.*)/(?P<searchstring>.+)', 'member.views_api.get', name='api_get'),
+    url(r'^api/list/', 'member.views_api.list', name='api_list'),
+    url(r'^api/filter/(?P<field>.*)/(?P<searchstring>.+)', 'member.views_api.filter', name='api_filter'),
+    url(r'^api/get/(?P<field>.*)/(?P<searchstring>.+)', 'member.views_api.get', name='api_get'),
 
 )
