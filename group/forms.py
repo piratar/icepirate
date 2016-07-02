ac6396da5dd84d0e9f2702d1874859aeb2921504
Bjarni R. Einarsson <bre@klaki.net>
2016-07-02 02:21:51 +0000

    Add subgroups and locations to group add/edit form

--- group/forms.py
+++ group/forms.py
@@ -1,9 +1,14 @@
 from django.forms import ModelForm
+from django.forms import CheckboxSelectMultiple
+from django.forms import ModelMultipleChoiceField
 
 from group.models import Group
+from locationcode.models import LocationCode
 
 class GroupForm(ModelForm):
+    auto_subgroups = ModelMultipleChoiceField(required=False, widget=CheckboxSelectMultiple(), queryset=Group.objects.all())
+    auto_locations = ModelMultipleChoiceField(required=False, queryset=LocationCode.objects.all())
+
     class Meta:
         model = Group
-        fields = ['name', 'email', 'added']
-
+        fields = ['name', 'email', 'added', 'auto_subgroups', 'auto_locations']
