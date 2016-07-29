b18fa8ed9c82738a52cff06a5cadd948a968b298
Bjarni R. Einarsson <bre@klaki.net>
2016-07-29 01:33:23 +0000

    Create `manage.py national_registry` for lookups and updates

---
+++ icepirate/management/commands/national_registry.py
@@ -0,0 +1,97 @@
+import datetime
+import pytz
+import sys
+import json
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+
+from icepirate.utils import lookup_national_registry
+from icepirate.utils import merge_national_registry_info
+from member.models import Member
+
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument('--lack-muni', action='store_true', dest='lack-muni')
+        parser.add_argument('--dump-ssns', action='store_true', dest='dump-ssns')
+        parser.add_argument('--update-as-json', action='store_true', dest='update-as-json')
+        parser.add_argument('--update-db', action='store_true', dest='update-db')
+        parser.add_argument('--intersect', action='store_true', dest='intersect')
+        parser.add_argument('--ssn', nargs='*', dest='ssn')
+        parser.add_argument('--refresh', nargs='*', dest='refresh', type=str)
+        for arg in ('url', 'password', 'username', 'xml_namespace'):
+            parser.add_argument('--%s' % arg, nargs='*', type=str, dest=arg)
+
+    def handle(self, *args, **options):
+
+        # These arguments let us override the Django settings from
+        # the CLI - necessary while we don't want to enable the new
+        # gateway completely.
+        for arg in ('url', 'password', 'username', 'xml_namespace'):
+            if options.get(arg):
+                settings.NATIONAL_REGISTRY[arg] = options.get(arg)[0]
+
+        # Each of the selection arguments creates a set of users...
+
+        # These are the users that lack a legal municipality code.
+        user_sets = []
+        if options.get('lack-muni', False):
+            user_sets.append(
+               set(Member.objects.filter(legal_municipality_code=None)) |
+               set(Member.objects.filter(legal_municipality_code='')))
+
+        # These are users whose SSN we have specified manually.
+        ssn = []
+        for ssns in (options.get('ssn') or []):
+            ssn.extend([s.strip().replace('-', '') for s in ssns.split(',')])
+        if ssn:
+            user_sets.append(
+                set(Member.objects.filter(ssn__in=ssn)))
+
+        # These are users whose last legal-lookup happened before a deadline.
+        # An example of usage: --refresh=$(date +%s '6 months ago')
+        if options.get('refresh'):
+            deadline_set = set()
+            for deadline in options.get('refresh'):
+                when = datetime.datetime.utcfromtimestamp(int(deadline))
+                when = when.replace(tzinfo=pytz.utc)
+                deadline_set |= (
+                   set(Member.objects.filter(legal_lookup_timing__lt=when)))
+            users_sets.append(deadline_set)
+
+        # The specified user sets are combined, either by intersecting them
+        # (constraint AND constraint AND ...) or combining (OR).
+        users = user_sets.pop(0)
+        for uset in user_sets:
+            if options.get('intersect', False):
+                users &= uset
+            else:
+                users |= uset
+
+        if not users:
+            sys.stderr.write('No users, nothing to do.\n')
+
+        else:
+            sys.stderr.write('Processing %d users...\n' % len(users))
+            if options.get('dump-ssns'):
+                print '\n'.join(user.ssn for user in sorted(users))
+
+            dump = []
+            if options.get('update-db') or options.get('update-as-json'):
+                for user in users:
+                    nr_info = lookup_national_registry(user.ssn)
+
+                    if options.get('update-db'):
+                        sys.stderr.write('Updating %s in DB\n' % user.ssn)
+                        now = timezone.now()
+                        merge_national_registry_info(user, nr_info, now)
+                        user.save()
+
+                    if options.get('update-as-json'):
+                        dump.append(nr_info)
+
+            if dump:
+                json.dump(dump, sys.stdout)
--- icepirate/utils.py
+++ icepirate/utils.py
@@ -2,6 +2,12 @@
 import hashlib
 import json
 import os
+import sys
+import time
+import urllib
+import urllib2
+from lxml import etree
+from StringIO import StringIO
 
 from django.conf import settings
 from django.core.mail import send_mail
@@ -9,6 +15,7 @@ from django.http import HttpResponse
 from django.utils.translation import ugettext as _
 
 import re
+
 
 def validate_ssn(ssn):
     # Must be precisely 10 numerical digits
@@ -76,4 +83,135 @@ def json_error(exception):
     }
     return HttpResponse(json.dumps(response_data), content_type='application/json')
 
+def get_http_response(web_url, data=None, quit_on_failure=False):
+    retry_count = 2
+    success = False
+    while not success and retry_count > 0:
+        try:
+            response = urllib2.urlopen(web_url, data=data, timeout=5)
+            success = True
+        except urllib2.HTTPError:
+            break
+        except IOError, e:
+            sys.stderr.write('Error, will retry: %s\n' % e)
+            retry_count -= 1
+            time.sleep(1)
 
+    if success:
+        return response
+    else:
+        sys.stderr.write('Error: Failed retrieving URL: %s\n' % web_url)
+        if quit_on_failure:
+            quit(1)
+        raise
+
+def lookup_national_registry_lt(ssn):
+    nr_url = settings.NATIONAL_REGISTRY['url']
+    nr_username = settings.NATIONAL_REGISTRY['username']
+    nr_password = settings.NATIONAL_REGISTRY['password']
+    nr_xml_namespace = settings.NATIONAL_REGISTRY['xml_namespace']
+
+    nr_data = urllib.urlencode({
+        'username': nr_username,
+        'password': nr_password,
+        'regno': ssn,
+        'requesterregno': '',
+    })
+
+    response = get_http_response(nr_url, data=nr_data, quit_on_failure=True)
+    content = response.read()
+    #sys.stderr.write('%s => %s\n' % (ssn, content.decode('utf-8')))
+
+    # Parse the XML. Yes, it's painful to look at, and it should be, it's XML.
+    xmldoc = etree.parse(StringIO(content))
+    result = {
+        'is_individual': xmldoc.find('//ns:IsIndividual', namespaces={'ns': nr_xml_namespace}).text == 'true',
+        'is_current': xmldoc.find('//ns:IsCurrent', namespaces={'ns': nr_xml_namespace}).text == 'true',
+        'is_valid': xmldoc.find('//ns:IsValid', namespaces={'ns': nr_xml_namespace}).text == 'true',
+    }
+
+    if result['is_valid']:
+        result.update({
+            'name': xmldoc.find('//ns:FullName', namespaces={'ns': nr_xml_namespace}).text,
+            'legal_address': xmldoc.find('//ns:LegalAddress', namespaces={'ns': nr_xml_namespace}).text,
+            'legal_zip_code': xmldoc.find('//ns:LegalZipCode', namespaces={'ns': nr_xml_namespace}).text,
+            'legal_zone': xmldoc.find('//ns:LegalZone', namespaces={'ns': nr_xml_namespace}).text,
+            'legal_municipality_code': None
+        })
+
+    return result
+
+def lookup_national_registry_ferli(ssn):
+    nr_url = settings.NATIONAL_REGISTRY['url']
+    nr_password = settings.NATIONAL_REGISTRY['password']
+    nr_xml_namespace = settings.NATIONAL_REGISTRY['xml_namespace']
+
+    nr_data = urllib.urlencode({
+        'str_Ktala': unicode(ssn).replace('-', ''),
+        'str_Password': nr_password
+    })
+
+    try:
+        response = get_http_response(nr_url, data=nr_data, quit_on_failure=False)
+        content = response.read()
+        #sys.stderr.write('%s => %s\n' % (ssn, content.decode('utf-8')))
+        found_in_nr = True
+    except urllib2.HTTPError:
+        found_in_nr = False
+    except IOError, e:
+        quit(1)
+
+    # Parse the XML. Yes, it's painful to look at, and it should be, it's XML.
+    nsp = {'ns': nr_xml_namespace}
+    if found_in_nr:
+        xmldoc = etree.parse(StringIO(content))
+        tegrec = xmldoc.find('//ns:TegRec', namespaces=nsp)
+        kynkodi = xmldoc.find('//ns:KynKodi', namespaces=nsp)
+        retkod = xmldoc.find('//ns:Retkod', namespaces=nsp)
+    else:
+        tegrec = kynkodi = retkod = None
+
+    result = {
+        'is_individual': (kynkodi is not None) and (kynkodi.text.strip()) and True or False,
+        'is_current': (retkod is not None and '0' == retkod.text),
+        'is_valid': (retkod is not None and '0' == retkod.text),
+    }
+
+    if result['is_valid']:
+        result.update({
+            'name': xmldoc.find('//ns:Nafn', namespaces=nsp).text,
+            'legal_address': xmldoc.find('//ns:Heimili', namespaces=nsp).text,
+            'legal_zip_code': xmldoc.find('//ns:Postnr', namespaces=nsp).text,
+            'legal_zone': xmldoc.find('//ns:Sveitarfelag', namespaces=nsp).text,
+            'legal_municipality_code': xmldoc.find('//ns:SveitarfNum', namespaces=nsp).text,
+        })
+
+    return result
+
+def lookup_national_registry(ssn):
+    namespace = settings.NATIONAL_REGISTRY['xml_namespace']
+
+    # FIXME: This seems a bit silly.
+    if 'ws.lt.is' in namespace:
+        return lookup_national_registry_lt(ssn)
+
+    elif 'xml.ferli.is' in namespace:
+        return lookup_national_registry_ferli(ssn)
+
+    raise NotImplementedError('Unknown XML namespace, halp')
+
+def merge_national_registry_info(member, nr_info, now):
+    assert(nr_info.get('name') and nr_info.get('legal_address'))
+
+    member.legal_name = nr_info['name']
+    member.legal_address = nr_info['legal_address']
+
+    if nr_info.get('legal_zip_code'):
+        member.legal_zip_code = nr_info['legal_zip_code']
+    if nr_info.get('legal_zone'):
+        member.legal_zone = nr_info['legal_zone']
+    if nr_info.get('legal_municipality_code'):
+        member.legal_municipality_code = nr_info['legal_municipality_code']
+
+    member.legal_lookup_timing = now
+    return member
