61490d690249688f588de01d907ed713faa23457
Helgi Hrafn Gunnarsson <helgi@binary.is>
2019-02-25 22:32:00 +0000

    Sendstats function fixed (got broken because of group -> membergroup change).

--- icepirate/management/commands/sendstats.py
+++ icepirate/management/commands/sendstats.py
@@ -3,7 +3,7 @@ import sys
 from django.core.management.base import BaseCommand
 from django.core.mail import send_mail
 
-from group.models import Group
+from member.models import MemberGroup
 from member.models import Member
 
 from icepirate.utils import quick_mail
@@ -12,16 +12,16 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        groups = Group.objects.all()
-        for group in groups:
-            subject = 'Statistics for group "%s"' % group.name
+        membergroups = MemberGroup.objects.all()
+        for membergroup in membergroups:
+            subject = 'Statistics for membergroup "%s"' % membergroup.name
             body = []
 
-            body.append('These are the member statistics for the group "%s"' % group.name)
-            body.append('Member count: %d' % group.members.count())
+            body.append('These are the member statistics for the membergroup "%s"' % membergroup.name)
+            body.append('Member count: %d' % membergroup.members.count())
 
-            sys.stdout.write('Sending member statistics for group \'%s\'...' % group.techname)
-            quick_mail(group.email, subject, '\n'.join(body))
+            sys.stdout.write('Sending member statistics for membergroup \'%s\'...' % membergroup.techname)
+            quick_mail(membergroup.email, subject, '\n'.join(body))
             sys.stdout.write(' done\n')
 
 
